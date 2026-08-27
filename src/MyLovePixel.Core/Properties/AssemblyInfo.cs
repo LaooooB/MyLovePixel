e4623f1442629fd40ee51bef20c97aa28735f391
LaooooB <billywenttocis@gmail.com>
2026-08-28 00:00:14 +0800

    feat(batch03): grant persistence controlled reconstruction access

--- src/MyLovePixel.Core/Properties/AssemblyInfo.cs
+++ src/MyLovePixel.Core/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("MyLovePixel.Commands")]
 [assembly: InternalsVisibleTo("MyLovePixel.Core.Tests")]
+[assembly: InternalsVisibleTo("MyLovePixel.Persistence")]
